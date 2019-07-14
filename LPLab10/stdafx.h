0e66b76ef93c81fae4ab582cdccf8ad35a3086c4
Nika <labovitch.veronque@gmail.com>
2019-07-14 16:42:26 +0300

    add folder

---
+++ LPLab10/stdafx.h
@@ -0,0 +1,22 @@
+// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
+// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
+// не часто измен€ютс€
+//
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cwchar>
+#include <string.h>
+
+
+
+// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
